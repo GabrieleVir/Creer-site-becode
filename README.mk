f42c2252e808fd6eefa058a212a60ad412ffe803
GabrieleVir <Gabriele_Virga@hotmail.com>
2017-04-07 12:34:04 +0200

    Commencement projet

---
+++ README.mk
@@ -0,0 +1,9 @@
+README.mk
+
+>Je vais mettre différents fichiers dont un appelé "Look" qui représentera en fichier MarkD le contenu de notre site.
+
+>Je rajouterai par la suite le fichier "Index.html" et c'est là qu'on s'occupera de tout ce qui est html.
+
+>Un fichier "CSS" pour tout ce qui est css
+
+>Et en dernier, le fichier "Jquerry" pour tout ce qui est animation du site. Le code jquerry se trouvera là.
